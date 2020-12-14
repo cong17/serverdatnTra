81910b6b86c7a96b9ee3b65d351c4aa5aa65be4e
cong17 <congkxtb98@gmail.com>
2020-12-14 07:34:00 +0700

    Update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cron = require('cron');
 const { getDataCovid, getDataNews } = require("./crawlerCovid");
 const fs = require('fs');
 const corsHeader = (req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -16,7 +16,7 @@ const corsHeader = (req, res, next) => {
     next();
   }
 };
-// app.use(corsHeader);
+app.use(corsHeader);
 
 
 const job = new cron.CronJob({
